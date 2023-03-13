dcdc323758b7f56c0e17ef942ae55401a222bdb2
Sebastião Félix <sebamorais0@gmail.com>
2023-03-13 17:58:52 -0300

    Add files via upload

--- focusTimer2/main.js
+++ focusTimer2/main.js
@@ -4,13 +4,30 @@ const seconds = document.querySelector('.segundos')
 const minuts = document.querySelector('.minutos')
 const increased = document.querySelector('.increased')
 const decreased = document.querySelector('.decreased')
+const arvore = document.querySelector('.florest')
+const florest = new Audio('./assets/audio/Floresta.wav')
+const chuva = document.querySelector('.rain')
+const rain = new Audio('./assets/audio/Chuva.wav')
+const fogo = document.querySelector('.fire')
+const fire = new Audio('./assets/audio/Lareira.wav')
+const mercado = document.querySelector('.market')
+const market = new Audio('./assets/audio/Cafeteria.wav')
 let minutos = Number(minuts.textContent)
 let stopTimer
+
+function removeClick(){
+  play.disabled = true;
+}
+
+function enableClick(){
+  play.disabled = false;
+}
 
 function tempoAtualizado(minutos, segundos){
   minuts.textContent = String(minutos).padStart(2, "0")
   seconds.textContent = String(segundos).padStart(2, "0")
 }
+
 
 function resetarTempo(){
   if(minutos > 25){
@@ -27,7 +44,11 @@ function countdown(){
       if( segundos <= 0 ) {
         segundos = 2
         --minutos
-      }      
+      }
+      
+      if(minutos < 0) {
+        return
+      }
   
       tempoAtualizado(minutos, segundos -1)
       
@@ -38,19 +59,56 @@ function countdown(){
   function parar(){
     resetarTempo()
     clearTimeout(stopTimer)
+    enableClick()
   }
 
   function timeIncreased(){
     minuts.innerHTML = minutos += 5;
+
+    tempoAtualizado(minutos, 0)
   }
 
   function timeDecreased(){
-    minuts.innerHTML = minutos -= 5;
+    if(minutos <= 0){
+      return
+    }
+    minuts.innerHTML = minutos -= 5;   
+
+    tempoAtualizado(minutos, 0)
   }
 
+  function playFlorest(){
+    florest.play();
+    rain.pause();
+    fire.pause();
+    market.pause();  
+  }
  
+  function playRain(){
+    rain.play();
+    florest.pause();
+    fire.pause();
+    market.pause();  
+  }
+
+  function playFire(){
+    fire.play();
+    florest.pause();
+    rain.pause();
+    market.pause();  
+  }
+
+  function playMarket(){
+    market.play();
+    florest.pause();
+    rain.pause();
+    fire.pause();
+  }
+
+  
 play.addEventListener('click', function(){
     countdown()
+    removeClick()
 },)
 
 stop.addEventListener('click', function(){
@@ -63,4 +121,20 @@ increased.addEventListener('click', function(){
 
 decreased.addEventListener('click', function(){
   timeDecreased()
+})
+
+arvore.addEventListener('click', function(){
+  playFlorest()
+})
+
+chuva.addEventListener('click', function(){
+  playRain()
+})
+
+fogo.addEventListener('click', function(){
+  playFire()
+})
+
+mercado.addEventListener('click', function(){
+  playMarket()
 })
